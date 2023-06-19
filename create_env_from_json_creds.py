2397d1e26c32b9efbb113a91420a79e16838806a
vlad t <vl.tsv@outlook.com>
2023-06-19 17:03:22 +0300

    cleaned

--- create_env_from_json_creds.py
+++ create_env_from_json_creds.py
@@ -7,7 +7,6 @@ with open(".env", "w") as f:
     f.write(f'GOOGLE_SHEET_API_CREDENTIALS={json.dumps(data)}')
 
 # todo: add prompt to also add spreadsheet id to .env
-
 # spreadsheet_id = input("Enter spreadsheet id: ")
 # with open(".env", "a") as f:
 #     f.write(f'GOOGLE_SHEET_API_SPREADSHEET_ID={spreadsheet_id}')
